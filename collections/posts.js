2b856ea121d0460aa0bbbb14336d1884be30ba9b
Sergey V. Kior <svkior@gmail.com>
2014-06-15 10:02:59 +0400

    Initial

---
+++ collections/posts.js
@@ -0,0 +1,4 @@
+/**
+ * Created by svkior on 14.06.14.
+ */
+Posts = new Meteor.Collection("posts");
---
+++ server/fixtures.js
@@ -0,0 +1,24 @@
+
+console.log(Posts.find().fetch());
+
+
+if(Posts.find().count() === 0) {
+
+    var LiveJournal = Meteor.require("livejournal");
+    var events = Async.runSync(function(done) {
+        LiveJournal.RPC.getevents({
+        	journal: 'engineering-ru',
+		auth_method: 'noauth',
+		selecttype: 'lastn',
+		howmany: 50
+	    }, function(err, value){
+
+	    done(null, value);
+	});
+     });
+
+    _.each(events.result.events, function(event){
+	    console.log(event);
+	    Posts.insert(event);
+    });
+}
