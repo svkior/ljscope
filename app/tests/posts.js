1df0a04a5a671486c37e941930038716727eaf7c
Sergey V. Kior <svkior@gmail.com>
2014-06-15 23:35:43 +0400

    Добавил 2 теста на проверку client-server и client2client

--- app/tests/posts.js
+++ app/tests/posts.js
@@ -17,4 +17,55 @@ suite('Posts', function () {
             done();
         })
     });
+
+    test('both on client and server', function (done, server, client) {
+
+        server.eval(function () {
+            Posts.find().observe({
+                added: addedNewPost
+            });
+
+            function addedNewPost(post) {
+                emit('post', post);
+            }
+        });
+
+        server.once('post', function (post) {
+            assert.equal(post.subject, 'hello subject');
+            done();
+        });
+
+        client.eval(function () {
+            Posts.insert({subject: 'hello subject'});
+        });
+
+    });
+
+    test('using two clients', function (done, server, c1, c2) {
+        c1.eval(function () {
+            Posts.find().observe({
+                added: addedNewPost
+            });
+
+            function addedNewPost(post) {
+                emit('post', post);
+            }
+
+            emit('done');
+        });
+
+        c1.once('post', function (post) {
+            assert.equal(post.subject, 'from c2');
+            done();
+        });
+
+        c1.once('done', function () {
+            c2.eval(insertPost);
+        });
+
+        function insertPost() {
+            Posts.insert({subject: 'from c2'});
+        }
+    });
+
 });
